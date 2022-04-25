62b461974c619531a08cb77c118f578e38c5eb70
Medkaffouh <kaffouhmohamed@gmail.com>
2022-04-25 21:49:37 +0000

    add function listener for drag events

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill Listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empties and call drag events
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0)
+}
+
+function dragEnd() {
+    this.className = 'fill';
+}
+
+function dragOver() {
+    console.log('over');
+}
+
+function dragEnter() {
+    console.log('enter');
+}
+
+function dragLeave() {
+    console.log('leave');
+}
+
+function dragDrop() {
+    console.log('drop');
+}
